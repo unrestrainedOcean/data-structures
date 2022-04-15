2f4dd3de033ab8dd9b13ce8e042fe98391661b5f
Rafael <makoralphie@yahoo.com>
2022-04-15 16:01:17 -0700

    pass all tests for the tree data structure problem

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,37 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = {};
+  newTree.counter = 0;
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
 var treeMethods = {};
 
+// Time complexity: O(1)
 treeMethods.addChild = function(value) {
+  var child = Tree(value);
+  this.children[this.counter] = child;
+  this.counter++;
 };
 
+// Time complexity: O(n)
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+
+  if (this.counter > 0) {
+    for (var key in this.children) {
+      if (this.children[key].contains(target)) {
+        return true;
+      }
+    }
+  }
+  return false;
+
 };
 
 
