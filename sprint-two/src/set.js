c56145e6bc48dcfb98588f80bddfde1511e37609
Rafael <makoralphie@yahoo.com>
2022-04-15 20:53:07 -0700

    to pass all tests for the set data structure problem

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,26 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
+// Time Complexity: linear
 setPrototype.add = function(item) {
+  if (!this._storage.includes(item)) {
+    this._storage.push(item);
+  }
 };
 
+// Time Complexity: linear
 setPrototype.contains = function(item) {
+  return this._storage.includes(item);
 };
 
+// Time Complexity: linear
 setPrototype.remove = function(item) {
+  this._storage = _.reject(this._storage, function(value) {
+    return value === item;
+  });
 };
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
